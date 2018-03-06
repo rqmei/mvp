6e197fe7a2e037c99d555c3d63937cddf3e3692b
rqmei <1280309704@qq.com>
2018-03-06 17:12:07 +0800

    1、网络管理工具类 2、转换工具类

--- TbLibrary/src/main/java/com/timingbar/safe/library/util/DataHelper.java
+++ TbLibrary/src/main/java/com/timingbar/safe/library/util/DataHelper.java
@@ -263,6 +263,25 @@ public class DataHelper {
     }
 
     /**
+     * 关闭IO
+     *
+     * @param closeables closeable
+     */
+    public static void closeIO(Closeable... closeables) {
+        if (closeables == null)
+            return;
+        try {
+            for (Closeable closeable : closeables) {
+                if (closeable != null) {
+                    closeable.close ();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace ();
+        }
+    }
+
+    /**
      * 获取mipmap下面的图片
      *
      * @param context 上下文
